e07753cd4acff977c2b04eef4d35916753cfbf2e
loveyousomuch <loveyousomuch554@gmail.com>
2020-06-27 19:46:01 +0300

    add some dependencies

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
+
+app.use(bodyParser.json())
+
+app.get('/api/users', (req, res) => {
+    let content = fs.readFileSync('users.json', 'utf-8')
+    let users = JSON.parse(content)
+
+    res.send(users)
+})
+
+app.listen(3000)
